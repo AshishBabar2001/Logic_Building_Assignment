d5795d1aa9a563b9000da8313fd55ecd4fb605a9
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-12-27 01:15:02 +0530

    Add files via upload

---
+++ 01 Accept Student Records And Display On Screen.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+    int Roll_No,Marks;
+
+    printf("\nEnter Roll Number & Marks : ");
+    scanf("%d %d",&Roll_No,&Marks);
+
+    printf("\nRollNo\tMarks ");
+    printf("\n%d\t%d",Roll_No,Marks);
+
+}
