592d42ede272a31981441a282beb9c238eaf592f
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-12-27 01:15:30 +0530

    Add files via upload

---
+++ 04 WAP Program Print Size Of Datatypes.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+    int i;
+    char Ch;
+    float d;
+
+    printf("\n Size Of Int : %d",sizeof(i));
+    printf("\n Size Of Char : %d",sizeof(Ch));
+    printf("\n Size Of float : %d",sizeof(d));
+}
