1aca0fea93b52e3d49765f4a067dd6df56f10543
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-12-28 00:47:44 +0530

    Add files via upload

---
+++ 05 Write a Program to ASCII value of given character.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+    char ch='/0';
+
+    printf("\n Enter Character For See Its ASCII Value : ");
+    scanf("%c",&ch);
+
+    printf("\n ASCII Character Is ....");
+
+    printf("\n ASCII of %c = %d",ch,ch);
+
+    getch();
+}
