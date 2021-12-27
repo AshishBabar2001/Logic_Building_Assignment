1c882bc07dab4a21c7528957d482607bc3bae692
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-12-28 00:48:05 +0530

    Add files via upload

---
+++ 07 Display Number Is Even Or Odd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+    int n;
+    printf("Enter a Number : ");
+    scanf("%d",&n);
+    if(n % 2 == 0)
+    {
+        printf("\n Number Is Even");
+    }
+    else
+    {
+        printf("\n Number Is Odd");
+    }
+}
