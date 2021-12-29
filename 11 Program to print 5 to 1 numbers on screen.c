c26c84e351737a37e732416762eaa9280e69002f
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-12-30 01:50:55 +0530

    Add files via upload

---
+++ 11 Program to print 5 to 1 numbers on screen.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+ int Display()
+    {
+        int i = 0;
+        i = 5;
+    while(i>=1)
+    {
+        printf("\n%d",i);
+        i--;
+    }
+}
+int main()
+{
+ Display();
+
+ return 0;
+}
