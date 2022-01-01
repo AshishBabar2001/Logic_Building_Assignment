34b54391bd2af775409aaf5ca570b71237a56b43
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2022-01-01 17:23:06 +0530

    Add files via upload

---
+++ 16 ASCII Number To Character.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+    int No;
+
+    printf("Enter a Numnber To see Which ASCII Character IS : ");
+    scanf("%d",&No);
+
+    printf("\n ASCII of %d = %c",No,No);
+
+    getch();
+}
